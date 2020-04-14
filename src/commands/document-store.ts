cbd52ae5e32f06c02292a9857a32e0d30db2032a
Nebulis <nebounet@gmail.com>
2020-04-14 16:38:02 +0800

    feat: issue to document store

---
+++ src/commands/document-store.ts
@@ -0,0 +1,16 @@
+import { Argv } from "yargs";
+
+export const command = "document-store <method>";
+
+export const describe = "Invoke a function over a document store smart contract on the blockchain";
+
+export const builder = (yargs: Argv): Argv => yargs.commandDir("document-store", { extensions: ["ts", "js"] });
+
+export const handler = (): void => {};
+
+export default {
+  command,
+  describe,
+  builder,
+  handler
+};
