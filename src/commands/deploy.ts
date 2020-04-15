65671f753114da5b0cc0767e6d0826816b22201d
Nebulis <nebounet@gmail.com>
2020-04-15 18:00:27 +0800

    fix: resolve js extension

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -4,7 +4,7 @@ export const command = "deploy <contract-type>";
 
 export const describe = "Deploys a smart contract on the blockchain";
 
-export const builder = (yargs: Argv): Argv => yargs.commandDir("deploy", { extensions: ["ts"] });
+export const builder = (yargs: Argv): Argv => yargs.commandDir("deploy", { extensions: ["ts", "js"] });
 
 export const handler = (): void => {};
 
