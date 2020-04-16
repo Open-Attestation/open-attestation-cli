ba657c85e5abc9b7730cadf6cb101671c33066a7
Nebulis <nebounet@gmail.com>
2020-04-16 13:30:54 +0800

    chore: add warning when using key option

--- src/implementations/deploy/document-store/document-store.ts
+++ src/implementations/deploy/document-store/document-store.ts
@@ -1,14 +1,11 @@
-import { readFileSync } from "fs";
 import { DocumentStoreFactory } from "@govtechsg/document-store";
 import { getDefaultProvider, Wallet } from "ethers";
 import signale from "signale";
 import { DeployDocumentStoreCommand } from "../../../commands/deploy/deploy.types";
 import { getLogger } from "../../../logger";
-const { trace } = getLogger("deploy:document-store");
+import { getPrivateKey } from "../../private-key";
 
-export const getKeyFromFile = (file?: string): undefined | string => {
-  return file ? readFileSync(file).toString() : undefined;
-};
+const { trace } = getLogger("deploy:document-store");
 
 export const deployDocumentStore = async ({
   storeName,
@@ -16,9 +13,7 @@ export const deployDocumentStore = async ({
   key,
   keyFile
 }: DeployDocumentStoreCommand): Promise<{ contractAddress: string }> => {
-  const privateKey = key || getKeyFromFile(keyFile) || process.env["OA_PRIVATE_KEY"];
-  if (!privateKey)
-    throw new Error("No private key found in OA_PRIVATE_KEY, key or key-file, please supply at least one");
+  const privateKey = getPrivateKey({ key, keyFile });
   const provider = getDefaultProvider(network === "mainnet" ? "homestead" : network); // homestead => aka mainnet
   const factory = new DocumentStoreFactory(new Wallet(privateKey, provider));
   signale.await(`Sending transaction to pool`);
