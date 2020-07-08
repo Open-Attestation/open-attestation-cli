a72a01d989d63b8dd2a361e3d26065c0b48bc072
Nebulis <nebounet@gmail.com>
2020-07-08 16:24:08 +0800

    feat: add an option to add funds to a wallet at the creation

--- src/implementations/wallet/create.ts
+++ src/implementations/wallet/create.ts
@@ -6,8 +6,10 @@ import { highlight } from "../../utils";
 import fs from "fs";
 import { CreateWalletCommand } from "../../commands/wallet/wallet.type";
 import { progress as defaultProgress } from "../../implementations/utils/progress";
+import fetch from "node-fetch";
 
 export const create = async ({
+  fund,
   progress = defaultProgress("Encrypting Wallet"),
   outputFile,
 }: CreateWalletCommand & { progress?: (progress: number) => void }): Promise<string> => {
@@ -20,7 +22,17 @@ export const create = async ({
 
   const json = await wallet.encrypt(password, progress);
   const outputPath = path.resolve(outputFile);
-  signale.info(`Wallet with public address ${highlight(wallet.address)} successfully created. Find more details:`);
   fs.writeFileSync(outputPath, json);
+
+  if (fund === "ropsten") {
+    const response = await fetch(`https://faucet.ropsten.be/donate/${wallet.address}`).then((res) => res.json());
+    if (response.message) {
+      signale.warn(`[ropsten] Adding fund to ${wallet.address} failed: ${response.message}`);
+    } else {
+      signale.info(`[ropsten] Added fund to ${wallet.address}`);
+    }
+  }
+  signale.info(`Wallet with public address ${highlight(wallet.address)} successfully created. Find more details:`);
+
   return outputPath;
 };
