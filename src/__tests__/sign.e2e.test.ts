3c2e5e1f4ad8d3bb5ee7cde671edaa48eb421f82
Raymond Yeh <ray@geek.sg>
2020-10-05 17:37:54 +0800

    fix: integration test for did signing

--- src/__tests__/sign.e2e.test.ts
+++ src/__tests__/sign.e2e.test.ts
@@ -1,150 +1,54 @@
-import { sign, Output } from "../implementations/sign";
+import { sign } from "../implementations/sign";
 import fs from "fs";
 import path from "path";
 import tmp from "tmp";
 
 const fixtureFolderName = "fixture";
-const wrappedFileName = `${fixtureFolderName}/wrapped-open-attestation-document.json`;
-const wrappedFileNameTwo = `${fixtureFolderName}/wrapped-open-attestation-document-2.json`;
+const wrappedFileName1 = `${fixtureFolderName}/unsigned-did.json`;
+const wrappedFileName2 = `${fixtureFolderName}/unsigned-dnsDid.json`;
 
 describe("sign", () => {
-  describe("sign with directory input", () => {
-    // eslint-disable-next-line jest/no-hooks
-    it("should sign document when folder contains one valid open attestation document", async () => {
-      const inputDirectory = tmp.dirSync();
-      const outputDirectory = tmp.dirSync();
-      fs.copyFileSync(
-        path.resolve(__dirname, wrappedFileName),
-        path.resolve(__dirname, `${inputDirectory.name}/wrapped-open-attestation-document.json`)
-      );
+  it("should sign documents when folder contain multiple valid open attestation documents", async () => {
+    const inputDirectory = tmp.dirSync();
+    const outputDirectory = tmp.dirSync();
+    fs.copyFileSync(
+      path.resolve(__dirname, wrappedFileName1),
+      path.resolve(__dirname, `${inputDirectory.name}/did.json`)
+    );
+    fs.copyFileSync(
+      path.resolve(__dirname, wrappedFileName2),
+      path.resolve(__dirname, `${inputDirectory.name}/dnsDid.json`)
+    );
 
-      await sign({
-        rawDocumentsPath: inputDirectory.name,
-        outputPath: outputDirectory.name,
-        outputPathType: Output.Directory,
-        privateKey: "0x0123456789012345678901234567890123456789012345678901234567890123",
-        publicKey: "0x14791697260E4c9A71f18484C9f997B308e59325",
-      });
-
-      const file = JSON.parse(
-        fs.readFileSync(`${outputDirectory.name}/wrapped-open-attestation-document.json`, { encoding: "utf8" })
-      );
-
-      expect(file).toMatchObject({
-        proof: {
-          proofPurpose: "assertionMethod",
-          signature:
-            "0x24ffefc58988bff46074b847b1b47973a39658fde92a845ab51634f5ed4831e047dd8107648761d3b916d603712242871bdccc74437d48a5f57c9b78bfd180281c",
-          type: "EcdsaSecp256k1Signature2019",
-          verificationMethod: "0x14791697260E4c9A71f18484C9f997B308e59325",
-        },
-      });
-    });
-    it("should sign documents when folder contain multiple valid open attestation documents", async () => {
-      const inputDirectory = tmp.dirSync();
-      const outputDirectory = tmp.dirSync();
-      fs.copyFileSync(
-        path.resolve(__dirname, wrappedFileName),
-        path.resolve(__dirname, `${inputDirectory.name}/wrapped-open-attestation-document-1.json`)
-      );
-      fs.copyFileSync(
-        path.resolve(__dirname, wrappedFileNameTwo),
-        path.resolve(__dirname, `${inputDirectory.name}/wrapped-open-attestation-document-2.json`)
-      );
-
-      await sign({
-        rawDocumentsPath: inputDirectory.name,
-        outputPath: outputDirectory.name,
-        outputPathType: Output.Directory,
-        privateKey: "0x0123456789012345678901234567890123456789012345678901234567890123",
-        publicKey: "0x14791697260E4c9A71f18484C9f997B308e59325",
-      });
-
-      const file1 = JSON.parse(
-        fs.readFileSync(`${outputDirectory.name}/wrapped-open-attestation-document-1.json`, { encoding: "utf8" })
-      );
-      const file2 = JSON.parse(
-        fs.readFileSync(`${outputDirectory.name}/wrapped-open-attestation-document-2.json`, { encoding: "utf8" })
-      );
-
-      expect(file1).toMatchObject({
-        proof: {
-          proofPurpose: "assertionMethod",
-          signature:
-            "0x24ffefc58988bff46074b847b1b47973a39658fde92a845ab51634f5ed4831e047dd8107648761d3b916d603712242871bdccc74437d48a5f57c9b78bfd180281c",
-          type: "EcdsaSecp256k1Signature2019",
-          verificationMethod: "0x14791697260E4c9A71f18484C9f997B308e59325",
-        },
-      });
-
-      expect(file2).toMatchObject({
-        proof: {
-          proofPurpose: "assertionMethod",
-          signature:
-            "0xf4bd905c2ebf829349ffd1cd451bcca9e6ec0d1bb2b16ad0649a2406b271d719642c3825b68102369a9ea9ce355133e5e24a9ef0525b33198b2d4938e474f87d1b",
-          type: "EcdsaSecp256k1Signature2019",
-          verificationMethod: "0x14791697260E4c9A71f18484C9f997B308e59325",
-        },
-      });
-    });
-  });
-
-  describe("sign with file input", () => {
-    it("should output as file when input path is a file", async () => {
-      const inputDirectory = tmp.dirSync();
-      const outputFile = tmp.fileSync();
-      fs.copyFileSync(
-        path.resolve(__dirname, wrappedFileName),
-        path.resolve(__dirname, `${inputDirectory.name}/wrapped-open-attestation-document.json`)
-      );
-
-      await sign({
-        rawDocumentsPath: path.resolve(inputDirectory.name, "wrapped-open-attestation-document.json"),
-        outputPath: outputFile.name,
-        outputPathType: Output.File,
-        privateKey: "0x0123456789012345678901234567890123456789012345678901234567890123",
-        publicKey: "0x14791697260E4c9A71f18484C9f997B308e59325",
-      });
-
-      const file = JSON.parse(fs.readFileSync(`${outputFile.name}`, { encoding: "utf8" }));
-
-      expect(file).toMatchObject({
-        proof: {
-          proofPurpose: "assertionMethod",
-          signature:
-            "0x24ffefc58988bff46074b847b1b47973a39658fde92a845ab51634f5ed4831e047dd8107648761d3b916d603712242871bdccc74437d48a5f57c9b78bfd180281c",
-          type: "EcdsaSecp256k1Signature2019",
-          verificationMethod: "0x14791697260E4c9A71f18484C9f997B308e59325",
-        },
-      });
+    await sign({
+      rawDocumentsPath: inputDirectory.name,
+      outputDir: outputDirectory.name,
+      key: "0x0000000000000000000000000000000000000000000000000000000000000003",
+      publicKey: "did:ethr:0x6813Eb9362372EEF6200f3b1dbC3f819671cBA69#controller",
+      algorithm: "Secp256k1VerificationKey2018",
     });
 
-    it("should allow output as StdOut when input path is a file", async () => {
-      let stdOut: any;
-      jest.spyOn(console, "log").mockImplementation((input) => {
-        stdOut = input;
-      });
-      const inputDirectory = tmp.dirSync();
-      fs.copyFileSync(
-        path.resolve(__dirname, wrappedFileName),
-        path.resolve(__dirname, `${inputDirectory.name}/wrapped-open-attestation-document.json`)
-      );
-      await sign({
-        rawDocumentsPath: path.resolve(inputDirectory.name, "wrapped-open-attestation-document.json"),
-        outputPathType: Output.StdOut,
-        privateKey: "0x0123456789012345678901234567890123456789012345678901234567890123",
-        publicKey: "0x14791697260E4c9A71f18484C9f997B308e59325",
-      });
+    const file1 = JSON.parse(fs.readFileSync(`${outputDirectory.name}/did.json`, { encoding: "utf8" }));
+    const file2 = JSON.parse(fs.readFileSync(`${outputDirectory.name}/dnsDid.json`, { encoding: "utf8" }));
 
-      expect(JSON.parse(stdOut)).toMatchObject({
-        proof: {
-          proofPurpose: "assertionMethod",
-          signature:
-            "0x24ffefc58988bff46074b847b1b47973a39658fde92a845ab51634f5ed4831e047dd8107648761d3b916d603712242871bdccc74437d48a5f57c9b78bfd180281c",
-          type: "EcdsaSecp256k1Signature2019",
-          verificationMethod: "0x14791697260E4c9A71f18484C9f997B308e59325",
-        },
-      });
-    });
+    expect(file1.proof).toMatchObject([
+      {
+        proofPurpose: "assertionMethod",
+        signature:
+          "0x6d0ff5c64b8230cdc471f38267495002f2c762acf7a80250599809ee32b4255377f1adcb56fb712dee66bfeb21be6b5d802f299aea1f1edca129e88e4c1742ce1c",
+        type: "OpenAttestationSignature2018",
+        verificationMethod: "did:ethr:0x6813Eb9362372EEF6200f3b1dbC3f819671cBA69#controller",
+      },
+    ]);
+
+    expect(file2.proof).toMatchObject([
+      {
+        proofPurpose: "assertionMethod",
+        signature:
+          "0x04b56b7a8405364cc925143c2fc28f91ee7a3e340402ba66e616f275110cbc446e140716f88f418581e02a371d246e1b98f07c937ee9b96d7bc8aebb470345c71b",
+        type: "OpenAttestationSignature2018",
+        verificationMethod: "did:ethr:0x6813Eb9362372EEF6200f3b1dbC3f819671cBA69#controller",
+      },
+    ]);
   });
 });
--- src/implementations/sign/index.ts
+++ src/implementations/sign/index.ts
@@ -1,6 +1,6 @@
 import { PrivateKeyOption } from "../../commands/shared";
 import { getPrivateKey } from "../utils/wallet";
-import { documentsInDirectory, readOpenAttestationFile, writeDocumentToDisk } from "../utils/disk";
+import { documentsInDirectory, readOpenAttestationFile, writeDocumentToDisk, isDir } from "../utils/disk";
 import { signDocument, defaultSigners } from "@govtechsg/oa-did-sign";
 import path from "path";
 import mkdirp from "mkdirp";
@@ -17,6 +17,7 @@ export const supportedAlgorithms = Array.from(defaultSigners.keys());
 export const sign = async ({ rawDocumentsPath, outputDir, publicKey, algorithm, key, keyFile }: SignCommand) => {
   const privateKey = await getPrivateKey({ key, keyFile });
   if (!privateKey) throw new Error("Private key is not specified (use -k or -f to specify key)");
+  if (!isDir(outputDir)) throw new Error("output-dir must be a director");
 
   // Create output dir
   if (outputDir) {
