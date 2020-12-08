40aa2647c335a6d95854d36d72c8934f95455c23
justussoh <justus.soh@hotmail.com>
2020-12-08 11:55:41 +0800

    feat: added deploy paymaster

---
+++ src/commands/deploy/paymaster.ts
@@ -0,0 +1,36 @@
+import { Argv } from "yargs";
+import { error, success, info } from "signale";
+import { getLogger } from "../../logger";
+import { deployPaymaster } from "../../implementations/deploy/paymaster";
+import { DeployPaymasterCommand } from "./deploy.types";
+import { withGasPriceOption, withNetworkAndKeyOption } from "../shared";
+import { getEtherscanAddress } from "../../utils";
+
+const { trace } = getLogger("deploy:paymaster");
+
+export const command = "paymaster <paymaster-name> [options]";
+
+export const describe = "Deploys a naive paymaster contract on the blockchain";
+
+export const builder = (yargs: Argv): Argv =>
+  withGasPriceOption(
+    withNetworkAndKeyOption(
+      yargs.positional("paymaster-name", {
+        description: "Name of the paymaster",
+        normalize: true,
+      })
+    )
+  );
+
+export const handler = async (args: DeployPaymasterCommand): Promise<string | undefined> => {
+  trace(`Args: ${JSON.stringify(args, null, 2)}`);
+  try {
+    info(`Deploying paymaster ${args.paymasterName}`);
+    const paymaster = await deployPaymaster(args);
+    success(`Paymaster ${args.paymasterName} deployed at ${paymaster.contractAddress}`);
+    info(`Find more details at ${getEtherscanAddress({ network: args.network })}/address/${paymaster.contractAddress}`);
+    return paymaster.contractAddress;
+  } catch (e) {
+    error(e.message);
+  }
+};
