0c9d4815a6c8716ad80d1e19bf56d5263645d71d
Sim Boon Long <simboonlong@gmail.com>
2021-12-22 15:40:14 +0800

    refactor: get config

--- src/implementations/config/helpers.ts
+++ src/implementations/config/helpers.ts
@@ -1,4 +1,4 @@
-import { Issuer, RevocationType } from "@govtechsg/open-attestation/dist/types/2.0/types";
+import { v2 } from "@govtechsg/open-attestation";
 import fetch from "node-fetch";
 import { info, success } from "signale";
 import { highlight } from "../../utils";
@@ -7,9 +7,20 @@ import { readFile } from "../../implementations/utils/disk";
 import { deployDocumentStore } from "../../implementations/deploy/document-store";
 import { deployTokenRegistry } from "../../implementations/deploy/token-registry";
 
+interface UpdatedWallet {
+  configFile: ConfigFile;
+  walletStr: string;
+}
+
+export const getConfigWithUpdatedWallet = ({ configFile, walletStr }: UpdatedWallet): ConfigFile => {
+  return {
+    ...configFile,
+    wallet: { ...configFile.wallet, encryptedJson: walletStr },
+  };
+};
+
 interface UpdatedForms {
-  forms: Form[];
-  walletAddress: string;
+  configFile: ConfigFile;
   documentStoreAddress: string;
   tokenRegistryAddress: string;
   dnsVerifiable: Dns;
@@ -17,43 +28,55 @@ interface UpdatedForms {
   dnsTransferableRecord: Dns;
 }
 
-// TODO: can do as another cli feature? return config file with updated forms (validate issuer dns meanwhile)
-export const getUpdateForms = ({
-  walletAddress,
-  forms,
+export const getConfigWithUpdatedForms = ({
+  configFile,
   documentStoreAddress,
   tokenRegistryAddress,
   dnsVerifiable,
   dnsDid,
   dnsTransferableRecord,
-}: UpdatedForms): Form[] => {
-  forms.map((form: Form) => {
+}: UpdatedForms): ConfigFile => {
+  const { wallet, forms } = configFile;
+  const { encryptedJson } = wallet;
+  const { address } = JSON.parse(encryptedJson);
+
+  const updatedForms = forms.map((form: Form) => {
     if (form.type === "VERIFIABLE_DOCUMENT") {
-      const updatedIssuers = form.defaults.issuers.map((issuer: Issuer) => {
-        if (issuer.identityProof?.type === "DNS-TXT") {
-          issuer.documentStore = documentStoreAddress;
-          if (issuer.identityProof?.location) issuer.identityProof.location = dnsVerifiable;
-        } else if (issuer.identityProof?.type.includes("DID")) {
-          issuer.id = `did:ethr:0x${walletAddress}`;
-          if (issuer.revocation?.type) issuer.revocation.type = "NONE" as RevocationType;
-          if (issuer.identityProof?.location) issuer.identityProof.location = dnsDid;
-          if (issuer.identityProof?.key) issuer.identityProof.key = `did:ethr:0x${walletAddress}#controller`;
+      const updatedIssuers = form.defaults.issuers.map((issuer) => {
+        if (issuer.identityProof) {
+          if (issuer.identityProof.type === "DNS-TXT") {
+            issuer.documentStore = documentStoreAddress;
+            issuer.identityProof.location = dnsVerifiable;
+          } else if (issuer.identityProof.type === "DID" || issuer.identityProof.type === "DNS-DID") {
+            issuer.id = `did:ethr:0x${address}`;
+            issuer.identityProof.location = dnsDid;
+            issuer.identityProof.key = `did:ethr:0x${address}#controller`;
+            if (issuer.revocation) {
+              issuer.revocation.type = "NONE" as v2.RevocationType;
+            }
+          }
         }
         return issuer;
       });
       form.defaults.issuers = updatedIssuers;
     }
+
     if (form.type === "TRANSFERABLE_RECORD") {
-      const updatedIssuers = form.defaults.issuers.map((issuer: Issuer) => {
+      const updatedIssuers = form.defaults.issuers.map((issuer) => {
         issuer.tokenRegistry = tokenRegistryAddress;
         if (issuer.identityProof?.location) issuer.identityProof.location = dnsTransferableRecord;
         return issuer;
       });
       form.defaults.issuers = updatedIssuers;
     }
+
     return form;
   });
-  return forms;
+
+  return {
+    ...configFile,
+    forms: updatedForms,
+  };
 };
 
 export const getConfigFile = async (configTemplatePath: string, configTemplateUrl: string): Promise<ConfigFile> => {
@@ -104,7 +127,7 @@ export const validate = (forms: Form[]): boolean => {
   const isValidForm = forms.some((form: Form) => {
     const isValidFormType = form.type === "TRANSFERABLE_RECORD" && "VERIFIABLE_DOCUMENT";
     const isValidIdentityProofType = form.defaults.issuers.some(
-      (issuer: any) => issuer.identityProof?.type === "DNS-TXT" && "DNS-DID" && "DID"
+      (issuer) => issuer.identityProof?.type === "DNS-TXT" && "DNS-DID" && "DID"
     );
 
     return isValidFormType && isValidIdentityProofType;
