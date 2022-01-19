1b98f7aca91bbe1f8edba8dd598f0f7a6cfa9114
puayhiang <68947167+puayhiang@users.noreply.github.com>
2022-01-19 21:54:22 +0800

    fix: solidity error handling

--- src/implementations/title-escrow/changeHolder.ts
+++ src/implementations/title-escrow/changeHolder.ts
@@ -3,9 +3,10 @@ import { getLogger } from "../../logger";
 import { getWalletOrSigner } from "../utils/wallet";
 import { connectToTitleEscrow } from "./helpers";
 import { TitleEscrowChangeHolderCommand } from "../../commands/title-escrow/title-escrow-command.type";
-
 import { dryRunMode } from "../utils/dryRun";
 import { TransactionReceipt } from "@ethersproject/providers";
+import { getSolidityErrorHandler } from "../utils/solidityErrorHandling";
+import { getErrorMessage } from "../../utils";
 
 const { trace } = getLogger("title-escrow:changeHolder");
 
@@ -19,8 +20,12 @@ export const changeHolderOfTitleEscrow = async ({
   ...rest
 }: TitleEscrowChangeHolderCommand): Promise<TransactionReceipt> => {
   const wallet = await getWalletOrSigner({ network, ...rest });
+  const titleEscrow = await connectToTitleEscrow({ tokenId, address, wallet });
+  titleEscrow.callStatic.changeHolder(to).catch(function (err) {
+    signale.error(getErrorMessage(err));
+    process.exit(0);
+  });
   if (dryRun) {
-    const titleEscrow = await connectToTitleEscrow({ tokenId, address, wallet });
     await dryRunMode({
       gasPriceScale: gasPriceScale,
       estimatedGas: await titleEscrow.estimateGas.changeHolder(to),
@@ -30,10 +35,15 @@ export const changeHolderOfTitleEscrow = async ({
   }
   const gasPrice = await wallet.provider.getGasPrice();
   signale.await(`Sending transaction to pool`);
-  const titleEscrow = await connectToTitleEscrow({ tokenId, address, wallet });
-  const transaction = await titleEscrow.changeHolder(to, { gasPrice: gasPrice.mul(gasPriceScale) });
-  trace(`Tx hash: ${transaction.hash}`);
-  trace(`Block Number: ${transaction.blockNumber}`);
-  signale.await(`Waiting for transaction ${transaction.hash} to be mined`);
-  return transaction.wait();
+  try {
+    const transaction = await titleEscrow.changeHolder(to, { gasPrice: gasPrice.mul(gasPriceScale) });
+    trace(`Tx hash: ${transaction.hash}`);
+    trace(`Block Number: ${transaction.blockNumber}`);
+    signale.await(`Waiting for transaction ${transaction.hash} to be mined`);
+    return transaction.wait();
+  } catch (error) {
+    titleEscrow.interface;
+    const failureReason = await getSolidityErrorHandler(network)(error);
+    throw new Error(failureReason);
+  }
 };
