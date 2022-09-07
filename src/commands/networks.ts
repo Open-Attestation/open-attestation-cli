7283fd7db53fe0754bda804304ef08fb1264eaa2
Seth <hullogh@gmail.com>
2022-09-08 00:10:50 +0800

    refactor: remove redundant chain id

--- src/commands/networks.ts
+++ src/commands/networks.ts
@@ -1,7 +1,6 @@
 import { providers } from "ethers";
 
 type SupportedNetwork = {
-  chainId: number;
   explorer: string;
   provider: () => providers.Provider;
 };
@@ -30,37 +29,30 @@ export const supportedNetwork: {
   [key in NetworkCmdName]: SupportedNetwork;
 } = {
   [NetworkCmdName.Local]: {
-    chainId: 1337,
     explorer: "https://localhost/explorer",
     provider: jsonRpcProvider("http://127.0.0.1:8545"),
   },
   [NetworkCmdName.Mainnet]: {
-    chainId: 1,
     explorer: "https://etherscan.io",
     provider: defaultInfuraProvider("homestead"),
   },
   [NetworkCmdName.Ropsten]: {
-    chainId: 3,
     explorer: "https://ropsten.etherscan.io",
     provider: defaultInfuraProvider("ropsten"),
   },
   [NetworkCmdName.Rinkeby]: {
-    chainId: 4,
     explorer: "https://rinkeby.etherscan.io",
     provider: defaultInfuraProvider("rinkeby"),
   },
   [NetworkCmdName.Goerli]: {
-    chainId: 5,
     explorer: "https://goerli.etherscan.io",
     provider: defaultInfuraProvider("goerli"),
   },
   [NetworkCmdName.Polygon]: {
-    chainId: 137,
     explorer: "https://polygonscan.com",
     provider: defaultInfuraProvider("matic"),
   },
   [NetworkCmdName.Mumbai]: {
-    chainId: 80001,
     explorer: "https://mumbai.polygonscan.com",
     provider: defaultInfuraProvider("maticmum"),
   },
