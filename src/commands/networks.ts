5f8e52cb5acd59c60928d72089627bc111d0fbfb
Seth <hullogh@gmail.com>
2022-09-07 23:20:56 +0800

    refactor: provider helpers

--- src/commands/networks.ts
+++ src/commands/networks.ts
@@ -16,43 +16,53 @@ enum NetworkCmdName {
   Mumbai = "mumbai",
 }
 
+const defaultInfuraProvider =
+  (networkName: string): (() => providers.Provider) =>
+  () =>
+    new providers.InfuraProvider(networkName);
+
+const jsonRpcProvider =
+  (url: string): (() => providers.Provider) =>
+  () =>
+    new providers.JsonRpcProvider(url);
+
 export const supportedNetwork: {
   [key in NetworkCmdName]: SupportedNetwork;
 } = {
   [NetworkCmdName.Local]: {
     chainId: 1337,
     explorer: "https://localhost/explorer",
-    provider: () => new providers.JsonRpcProvider("http://127.0.0.1:8545"),
+    provider: jsonRpcProvider("http://127.0.0.1:8545"),
   },
   [NetworkCmdName.Mainnet]: {
     chainId: 1,
     explorer: "https://etherscan.io",
-    provider: () => new providers.InfuraProvider("homestead"),
+    provider: defaultInfuraProvider("homestead"),
   },
   [NetworkCmdName.Ropsten]: {
     chainId: 3,
     explorer: "https://ropsten.etherscan.io",
-    provider: () => new providers.InfuraProvider("ropsten"),
+    provider: defaultInfuraProvider("ropsten"),
   },
   [NetworkCmdName.Rinkeby]: {
     chainId: 4,
     explorer: "https://rinkeby.etherscan.io",
-    provider: () => new providers.InfuraProvider("rinkeby"),
+    provider: defaultInfuraProvider("rinkeby"),
   },
   [NetworkCmdName.Goerli]: {
     chainId: 5,
     explorer: "https://goerli.etherscan.io",
-    provider: () => new providers.InfuraProvider("goerli"),
+    provider: defaultInfuraProvider("goerli"),
   },
   [NetworkCmdName.Polygon]: {
     chainId: 137,
     explorer: "https://polygonscan.com",
-    provider: () => new providers.InfuraProvider("matic"),
+    provider: defaultInfuraProvider("matic"),
   },
   [NetworkCmdName.Mumbai]: {
     chainId: 80001,
     explorer: "https://mumbai.polygonscan.com",
-    provider: () => new providers.InfuraProvider("maticmum"),
+    provider: defaultInfuraProvider("maticmum"),
   },
 };
 
