b92c4510be65119603bb53d47c8c4218c9e976dd
Seth <hullogh@gmail.com>
2022-09-07 22:51:37 +0800

    refactor: network name typing

--- src/commands/networks.ts
+++ src/commands/networks.ts
@@ -6,50 +6,50 @@ type SupportedNetwork = {
   provider: () => providers.Provider;
 };
 
-export enum NetworkCmdName {
-  local = "local",
-  mainnet = "mainnet",
-  ropsten = "ropsten",
-  rinkeby = "rinkeby",
-  goerli = "goerli",
-  polygon = "polygon",
-  mumbai = "mumbai",
+enum NetworkCmdName {
+  Local = "local",
+  Mainnet = "mainnet",
+  Ropsten = "ropsten",
+  Rinkeby = "rinkeby",
+  Goerli = "goerli",
+  Polygon = "polygon",
+  Mumbai = "mumbai",
 }
 
 export const supportedNetwork: {
   [key in NetworkCmdName]: SupportedNetwork;
 } = {
-  [NetworkCmdName.local]: {
+  [NetworkCmdName.Local]: {
     chainId: 1337,
     explorer: "https://localhost/explorer",
     provider: () => new providers.JsonRpcProvider(),
   },
-  [NetworkCmdName.mainnet]: {
+  [NetworkCmdName.Mainnet]: {
     chainId: 1,
     explorer: "https://etherscan.io",
     provider: () => new providers.InfuraProvider("homestead"),
   },
-  [NetworkCmdName.ropsten]: {
+  [NetworkCmdName.Ropsten]: {
     chainId: 3,
     explorer: "https://ropsten.etherscan.io",
     provider: () => new providers.InfuraProvider("ropsten"),
   },
-  [NetworkCmdName.rinkeby]: {
+  [NetworkCmdName.Rinkeby]: {
     chainId: 4,
     explorer: "https://rinkeby.etherscan.io",
     provider: () => new providers.InfuraProvider("rinkeby"),
   },
-  [NetworkCmdName.goerli]: {
+  [NetworkCmdName.Goerli]: {
     chainId: 5,
     explorer: "https://goerli.etherscan.io",
     provider: () => new providers.InfuraProvider("goerli"),
   },
-  [NetworkCmdName.polygon]: {
+  [NetworkCmdName.Polygon]: {
     chainId: 137,
     explorer: "https://polygonscan.com",
     provider: () => new providers.InfuraProvider("matic"),
   },
-  [NetworkCmdName.mumbai]: {
+  [NetworkCmdName.Mumbai]: {
     chainId: 80001,
     explorer: "https://mumbai.polygonscan.com",
     provider: () => new providers.InfuraProvider("maticmum"),
@@ -57,5 +57,5 @@ export const supportedNetwork: {
 };
 
 export const getSupportedNetwork = (networkCmdName: string): SupportedNetwork => {
-  return supportedNetwork[NetworkCmdName[networkCmdName as keyof typeof NetworkCmdName]];
+  return supportedNetwork[networkCmdName as NetworkCmdName];
 };
