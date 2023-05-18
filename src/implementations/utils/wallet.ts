1763111f54fa97f596fc3a6de765ca7d325cb24f
chershentan <tan_cher_shen@tech.gov.sg>
2023-05-18 13:44:45 +0800

    feat: add aws session-token option to withAwsKmsSignerOption (#261)
    * feat: add aws session-token option to withAwsKmsSignerOption
    
    * Update src/implementations/utils/wallet.ts
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/implementations/utils/wallet.ts
+++ src/implementations/utils/wallet.ts
@@ -60,6 +60,7 @@ export const getWalletOrSigner = async ({
       secretAccessKey: options.secretAccessKey, // credentials for your IAM user with KMS access
       region: options.region,
       keyId: options.kmsKeyId,
+      sessionToken: options.sessionToken,
     };
 
     const signer = new AwsKmsSigner(kmsCredentials).connect(provider);
